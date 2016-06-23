fa92b1c5c5d969a4562312f4150405fd3f2674b6
Vladislav Budnikov <vbud@softserveinc.com>
2016-06-23 13:46:17 +0300

    Adding testing class

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TVSetsStatePattern.V1;
+//using TVSetsStatePattern.V1;
+using TVSetsStatePattern.V4;
 
 namespace TVSetsStatePattern
 {
@@ -11,13 +12,22 @@ namespace TVSetsStatePattern
     {
         static void Main(string[] args)
         {
-            TVset tv = new TVset();
+            //TVset tv = new TVset();
+            //tv.ChannelUp();
+            //tv.SoundUp();
+
+
+            TVset tv = new TVset(new SwitchedOnState());
+
             tv.ChannelUp();
+
+            tv.ChannelUp();
+
             tv.SoundUp();
 
+            tv.Toggle();
 
-
-
+            tv.ChannelUp();
         }
     }
 }
