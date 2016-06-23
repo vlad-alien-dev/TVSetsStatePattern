ae01ea6476df3681f9eb549773629018b5995c62
Vladislav Budnikov <vbud@softserveinc.com>
2016-06-23 12:56:29 +0300

    Adding not-refactored object TVset

---
+++ V1/TVset.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVSetsStatePattern.V1
+{
+    public class TVset
+    {
+        private bool isSwitchedOn;
+        private int channel;
+        private int sound;
+
+        public void ChangeChannel()
+        {
+            if (isSwitchedOn)
+            {
+                SwitchChannel(channel);
+            }
+            else
+            {
+                SwitchOn();
+            }
+        }
+
+        public void ChangeSound()
+        {
+            if (isSwitchedOn)
+            {
+                AlterSound(sound);
+            }
+        }
+
+
+
+        private void SwitchChannel(int channel)
+        {
+            Console.WriteLine("Current channel is: {0}", channel);
+        }
+
+        private void SwitchOn()
+        {
+            isSwitchedOn = true;
+            Console.WriteLine("The TV is now switched on");
+        }
+
+        private void AlterSound(int sound)
+        {
+            Console.WriteLine("Current sound is: {0}", sound);
+        }
+    }
+}
